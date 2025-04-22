f54d75305d0d9f86990b43c080fa772673586b42
viniciuslsoares <viniciuslemesoares@gmail.com>
2025-04-22 18:39:45 +0000

    train scripts

---
+++ dev-seismic-byol/cli_finetune.py
@@ -0,0 +1,94 @@
+import argparse
+import torch
+from train import main
+from functions import *
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Fine-tuning script for seismic segmentation using pretrained BYOL backbone."
+    )
+
+    parser.add_argument(
+        "--pretrain_data",
+        type=str,
+        required=True,
+        default="f3",
+        help="Dataset used in pretraining (e.g., f3, seam_ai, both)"
+    )
+    parser.add_argument(
+        "--finetune_data",
+        type=str,
+        required=True,
+        default="f3",
+        help="Dataset used for fine-tuning (e.g., f3, seam_ai, both)"
+    )
+    parser.add_argument(
+        "--num_epochs", type=int, default=20, help="Number of training epochs"
+    )
+    parser.add_argument("--batch_size", type=int, default=8, help="Batch size")
+    parser.add_argument(
+        "--repetition", type=int, default=0, help="Experiment repetition index"
+    )
+    parser.add_argument(
+        "--learning_rate", type=float, default=0.001, help="Learning rate"
+    )
+    parser.add_argument(
+        "--cap",
+        type=float,
+        default=1.0,
+        help="Fraction of data to use for training (between 0 and 1)"
+    )
+    parser.add_argument(
+        "--freeze", action="store_true", help="Whether to freeze the encoder backbone"
+    )
+    parser.add_argument(
+        "--gpus", type=int, nargs="+", default=[0], help="List of GPU indices to use"
+    )
+
+    args = parser.parse_args()
+
+    PRETRAIN_LOGS_PATH = f"logs/train/{args.repetition}"
+    PRETRAIN_CKPT_PATH = f"ckpt/train/{args.repetition}"
+    IMPORT_ROOT_PATH = f"ckpt/pretrain/"
+
+    dataset_mapping = {
+        "seam_ai_N": "/workspaces/shared_data/seam_ai_datasets/seam_ai_N",
+        "seam_ai": "/workspaces/shared_data/seam_ai_datasets/seam_ai",
+        "f3": "/workspaces/shared_data/seismic/f3_segmentation",
+        "f3_N": "/workspaces/shared_data/seismic/f3_segmentation_N",
+        "both": "/workspaces/shared_data/seismic/both",
+        "both_N": "/workspaces/shared_data/seismic/both_N",
+    }
+
+    if args.finetune_data not in dataset_mapping.keys():
+        raise KeyError(f"Dataset '{args.finetune_data}' not found in available options: {list(dataset_mapping.keys())}")
+
+    pretrain_list = [
+        "f3", "f3_N", "seam_ai", "seam_ai_N", "both", "both_N", "s0", "a700", "imagenet", "coco", "sup",
+    ]
+
+    if args.pretrain_data not in pretrain_list:
+        raise KeyError(f"Pretrain '{args.pretrain_data}' not found in available options: {pretrain_list}")
+
+    # Simulação de treinamento
+    logger.info(" =-=-=- Beginning fine-tuning =-=-=-")
+    logger.info(f"Pretrain data: {args.pretrain_data}")
+    logger.info(f"Finetune data: {args.finetune_data}")
+    logger.info(f"Cap: {args.cap}, Freeze: {args.freeze}")
+
+    # Aqui você chama o método de treinamento
+    main(
+        pretrain_data=args.pretrain_data,
+        finetune_data=args.finetune_data,
+        data_path=dataset_mapping[args.finetune_data],
+        num_epochs=args.num_epochs,
+        batch_size=args.batch_size,
+        repetition=args.repetition,
+        learning_rate=args.learning_rate,
+        cap=args.cap,
+        freeze=args.freeze,
+        ckpt_path=PRETRAIN_CKPT_PATH,
+        logs_path=PRETRAIN_LOGS_PATH,
+        import_root_path=IMPORT_ROOT_PATH,
+        gpus=args.gpus,
+    )
---
+++ dev-seismic-byol/train.py
@@ -0,0 +1,144 @@
+import numpy as np
+from pathlib import Path
+
+from functions import *
+
+from minerva.transforms.transform import *
+from minerva.transforms.random_transform import *
+
+from minerva.data.readers import TiffReader, PNGReader
+from minerva.data.datasets import SimpleDataset
+from minerva.data.data_modules import MinervaDataModule
+
+from minerva.models.ssl.byol import BYOL
+from minerva.models.nets.image.deeplabv3 import DeepLabV3Backbone
+
+from minerva.pipelines.lightning_pipeline import SimpleLightningPipeline
+from lightning.pytorch.loggers.csv_logs import CSVLogger
+from lightning.pytorch.callbacks import ModelCheckpoint
+from lightning import Trainer
+
+
+def main(
+    pretrain_data,
+    finetune_data,
+    data_path,
+    num_epochs,
+    batch_size,
+    repetition,
+    learning_rate,
+    cap,
+    freeze,
+    ckpt_path,
+    logs_path,
+    import_root_path,
+    gpus,
+):
+
+    # Set general seed**
+    torch.manual_seed(repetition)
+    save_name = (
+        f"V{repetition}_pre_{pretrain_data}_train_{finetune_data}_cap_{cap*100:.0f}%"
+    )
+
+    # Transforms
+    if finetune_data == "f3" or finetune_data == "f3_N":
+        padding = Padding(256, 704)
+    elif finetune_data == "seam_ai" or finetune_data == "seam_ai_N":
+        padding = Padding(1008, 592)
+
+    # Dataset
+
+    image_path = f"{data_path}/images"
+    label_path = f"{data_path}/annotations"
+
+    train_data_reader = TiffReader(path=f"{image_path}/train")
+    train_label_reader = PNGReader(path=f"{label_path}/train")
+
+    val_data_reader = TiffReader(path=f"{image_path}/val")
+    val_label_reader = PNGReader(path=f"{label_path}/val")
+
+    train_dataset = SimpleDataset(
+        readers=[
+            train_data_reader,
+            train_label_reader,
+        ],
+        transforms=padding,
+        return_single=False,
+    )
+
+    val_dataset = SimpleDataset(
+        readers=[
+            val_data_reader,
+            val_label_reader,
+        ],
+        transforms=padding,
+        return_single=False,
+    )
+
+    # DataModule
+
+    data_module = CapDataModule(
+        cap_train=cap,
+        cap_val=1,
+        cap_test=1,
+        seed=repetition,
+        train_dataset=train_dataset,
+        val_dataset=val_dataset,
+        batch_size=batch_size,
+        drop_last=True,
+        shuffle_train=True,
+    )
+
+    # Model
+
+    model = get_model(
+        pretrain_data,
+        learning_rate,
+        freeze,
+        repetition,
+        import_root_path,
+    )
+
+    log_dir = Path(logs_path) / save_name / finetune_data
+    ckpt_dir = Path(ckpt_path) / save_name / finetune_data
+    logger = CSVLogger(log_dir, name=save_name, version=finetune_data)
+    ckpt_callback = ModelCheckpoint(
+        save_top_k=1, save_last=True, dirpath=ckpt_dir, monitor="val_IoU", mode="max"
+    )
+
+    trainer = Trainer(
+        accelerator="gpu",
+        logger=logger,
+        callbacks=[ckpt_callback],
+        max_epochs=num_epochs,
+        strategy="auto",
+        devices=gpus,
+    )
+
+    pipeline = SimpleLightningPipeline(
+        model=model,
+        trainer=trainer,
+        log_dir=log_dir,
+        save_run_status=True,
+    )
+
+    pipeline.run(data_module, task="fit")
+
+
+if __name__ == "__main__":
+    main(
+        pretrain_data="f3",
+        finetune_data="seam_ai",
+        data_path="/workspaces/shared_data/seam_ai_datasets/seam_ai",
+        num_epochs=20,
+        batch_size=8,
+        repetition=0,
+        learning_rate=0.001,
+        cap=0.1,
+        freeze=False,
+        ckpt_path="/workspaces/Seismic-Byol/dev-seismic-byol/ckpt",
+        logs_path="/workspaces/Seismic-Byol/dev-seismic-byol/logs",
+        import_root_path=None,
+        gpus=[0],
+    )
